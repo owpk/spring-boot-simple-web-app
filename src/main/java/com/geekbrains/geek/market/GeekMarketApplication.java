320e8ac750ca13f3f78951ce144a81eaf7963e34
owpk-q <vzvz4@mail.ru>
2021-01-31 06:14:54 +0900

    fix

--- src/main/java/com/geekbrains/geek/market/GeekMarketApplication.java
+++ src/main/java/com/geekbrains/geek/market/GeekMarketApplication.java
@@ -7,12 +7,6 @@ import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class GeekMarketApplication {
-	// Домашнее задание:
-	// 1. Добавить выгрузку всех продуктов через SOAP
-	// 2. * Добавить выгрузку заказов пользователя по его имени (защиту не ставим пока)
-
-	// Планы на следующие занятия:
-	// - Вернуться к вопросу об изменении цены товара перед оформлением заказа
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeekMarketApplication.class, args);
